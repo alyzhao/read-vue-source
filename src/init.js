b0dec4255cfa19b8eaa065c7335226d304790386
Allen Zhao <captainzhaoly@gmail.com>
2020-10-07 16:19:51 +0800

    使用ast描述dom数

--- src/init.js
+++ src/init.js
@@ -1,4 +1,5 @@
 import { initState } from './state';
+import { compileToFunctions } from './compiler/index';
 
 export function initMixin(Vue) {
   Vue.prototype._init = function (options) {
@@ -8,5 +9,25 @@ export function initMixin(Vue) {
     // 初始化状态, 劫持数据, 更新视图
     // vue 响应式数据原理
     initState(vm);
+
+    // 渲染模板
+    if (vm.$options.el) {
+      vm.$mount(vm.$options.el);
+    }
+  }
+
+  Vue.prototype.$mount = function (el) {
+    const vm = this;
+    const options = vm.$options;
+    el = document.querySelector(el);
+
+    if (!options.render) {
+      let { template } = options;
+      if (!template && el) {
+        template = el.outerHTML;
+      }
+      const render = compileToFunctions(template);
+      options.render = render;
+    }
   }
 }
