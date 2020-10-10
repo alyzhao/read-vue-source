5599c771b18cb65d9db64205161af911687a8b42
Allen Zhao <captainzhaoly@gmail.com>
2020-10-10 19:37:46 +0800

    更新dom时进行 diff

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import { renderMixin } from './vdom/index';
 import { initGlobalApi } from './global-api/index';
 import { stateMixin } from './state';
 
+import { compileToFunctions } from './compiler/index';
+import { patch, createElm } from './vdom/patch';
+
 function Vue(options) {
   this._init(options);
 }
@@ -16,5 +19,31 @@ stateMixin(Vue);
 
 // 静态方法
 initGlobalApi(Vue);
+
+const vm1 = new Vue({
+  data() {
+    return {
+      name: 'tom',
+    }
+  },
+});
+const render1 = compileToFunctions(`<div id="a" style="color: red;" class="a">{{name}}</div>`);
+const vnode1 = render1.call(vm1);
+document.body.appendChild(createElm(vnode1));
+
+const vm2 = new Vue({
+  data() {
+    return {
+      name: 'jerry',
+    }
+  },
+});
+const render2 = compileToFunctions(`<div id="b" style="background-color: blue;" class="b">{{name}}</div>`);
+const vnode2 = render2.call(vm2);
+
+setTimeout(() => {
+  patch(vnode1, vnode2);
+}, 1000)
+
 
 export default Vue;
--- src/vdom/patch.js
+++ src/vdom/patch.js
@@ -3,14 +3,36 @@
  * @param{Element} oldNode 这是之前的节点, 初次渲染时就是 选项 el
  */
 export function patch (oldNode, vnode) {
-  const el = createElm(vnode);
-  const parentNode = oldNode.parentNode; // https://developer.mozilla.org/zh-CN/docs/Web/API/Node/parentNode 获取父节点
-  parentNode.insertBefore(el, oldNode.nextSibling); // 插入节点
-  parentNode.removeChild(oldNode);
+  // 如果 nodeType 是 1 那么他是一个 Dom 元素也就是初次挂载的时候那么按照原来的流程执行即可
+
+  if (oldNode.nodeType === 1) {
+    const el = createElm(vnode);
+    const parentNode = oldNode.parentNode; // https://developer.mozilla.org/zh-CN/docs/Web/API/Node/parentNode 获取父节点
+    parentNode.insertBefore(el, oldNode.nextSibling); // 插入节点
+    parentNode.removeChild(oldNode);
+  } else {
+    // 这里来 diff 前后的虚拟节点, 在更新视图时
+    const oldEl = oldNode.el;
+
+    if (oldNode.tag !== vnode.tag) {
+      const el = createElm(vnode);
+      oldEl.parentNode.replaceChild(el, oldEl);
+    } else if (!oldNode.tag) {
+      // 都是文本节点, 对比文本内容是否一样, 不一样的话替换掉老的 dom 文本
+      if (oldNode.text !== vnode.text) {
+        oldEl.textContext = vnode.text;
+      }
+    } else {
+      // 标签一样需要比较属性和子元素, 此时复用老节点
+      const el = vnode.el = oldNode.el; // 此时当前虚拟节点的el用原来虚拟节点的el即可
+      updateProperties(vnode, oldNode.data);
+    }
+  }
+
   return el;
 }
 
-function createElm(vnode) {
+export function createElm(vnode) {
   const {
     tag,
     data,
@@ -31,17 +53,36 @@ function createElm(vnode) {
   return vnode.el;
 }
 
-function updateProperties(vnode) {
-  const { el, data = {} } = vnode;
-  for (let key in data) {
+function updateProperties(vnode, oldProps = {}) {
+  const {
+    el,
+    data: newProps = {},
+  } = vnode;
+
+  for (let key in oldProps) {
+    if (!newProps[key]) {
+      el.removeAttribute(key);
+    }
+  }
+
+  const newStyle = newProps.style;
+  const oldStyle = oldProps.style;
+
+  for (let key in oldStyle) {
+    if (!newStyle[key]) {
+      el.style[key] = '';
+    }
+  }
+
+  for (let key in newProps) {
     if (key === 'style') {
-      for (let styleName in data[key]) {
-        el.style[styleName] = data[key][styleName];
+      for (let styleName in newProps[key]) {
+        el.style[styleName] = newProps[key][styleName];
       }
     } else if (key === 'class') {
-      el.className = data[key];
+      el.className = newProps[key];
     } else {
-      el.setAttribute(key, data[key]);
+      el.setAttribute(key, newProps[key]);
     }
   }
 }
